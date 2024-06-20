cb5e2128b86903638063b6dcc0e4b3fec31801a8
Jen2022 <jennifer.g.fernandes@ucdconnect.ie>
2024-06-20 12:46:12 +0100

    Sport Tests

--- djangorestfw/backend/tests.py
+++ djangorestfw/backend/tests.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 from .models import CustomUser
 from .models import TeamCategory
+from .models import Sport
 
 class CustomUserViewSetTestCase(APITestCase):
     def setUp(self):
@@ -168,3 +169,67 @@ class TeamCategoryViewSetTestCase(APITestCase):
         self.assertIn('name', response.data)
         self.assertEqual(response.data['name'][0].code, 'invalid')
         self.assertEqual(str(response.data['name'][0]), 'A category with a similar name already exists.')
+
+class SportViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.admin_user = CustomUser.objects.create_user(
+            username='admin', password='password', email='admin@example.com',
+            fname='Admin', lname='User', user_type='admin'
+        )
+        self.client = APIClient()
+        self.sport_data = {'name': 'Golf'}
+        self.sport = Sport.objects.create(name='Golf')
+
+    def test_list_sports(self):
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.get(reverse('sport-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 8)
+        self.assertEqual(response.data[-1]['name'], 'golf')  # name should be normalized
+
+    def test_create_sport(self):
+        self.client.force_authenticate(user=self.admin_user)
+        new_sport_data = {'name': 'Baseball'}
+        response = self.client.post(reverse('sport-list'), new_sport_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Sport.objects.count(), 9)
+        self.assertEqual(Sport.objects.get(id=response.data['id']).name, 'baseball')  # name should be normalized
+    
+    def test_create_duplicate_sport(self):
+        self.client.force_authenticate(user=self.admin_user)
+        duplicate_sport_data = {'name': 'golf'}  # Same name, different case
+        response = self.client.post(reverse('sport-list'), duplicate_sport_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('name', response.data)
+        self.assertEqual(response.data['name'][0], 'sport with this name already exists.')
+
+    def test_create_duplicate_sport_with_different_case(self):
+        self.client.force_authenticate(user=self.admin_user)
+        duplicate_sport_data = {'name': 'GOLF'}  # Same name, different case
+        response = self.client.post(reverse('sport-list'), duplicate_sport_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('name', response.data)
+        self.assertEqual(response.data['name'][0], 'A sport with a similar name already exists.')
+
+    
+    def test_update_sport(self):
+        self.client.force_authenticate(user=self.admin_user)
+        update_data = {'name': 'Baseball'}
+        response = self.client.patch(reverse('sport-detail', kwargs={'pk': self.sport.pk}), update_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'baseball')  # name should be normalized
+
+    def test_delete_sport(self):
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.delete(reverse('sport-detail', kwargs={'pk': self.sport.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Sport.objects.count(), 7)
+
+    def test_normalize_sport_name(self):
+        self.client.force_authenticate(user=self.admin_user)
+        new_sport_data = {'name': ' SoCCer '}
+        response = self.client.post(reverse('sport-list'), new_sport_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('name', response.data)
+        self.assertEqual(response.data['name'][0].code, 'invalid')
+        self.assertEqual(str(response.data['name'][0]), 'A sport with a similar name already exists.')
